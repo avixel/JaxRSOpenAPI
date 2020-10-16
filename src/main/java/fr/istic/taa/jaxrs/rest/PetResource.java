84d8098a728eaee5cb3395abb38e23db3d17f5ee
Allan <allan.vixel@etudiant.univ-rennes1.fr>
2020-10-16 16:22:04 +0200

    maj tp fin

--- src/main/java/fr/istic/taa/jaxrs/rest/PetResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/PetResource.java
@@ -1,6 +1,12 @@
 package fr.istic.taa.jaxrs.rest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,21 +18,38 @@ import fr.istic.taa.jaxrs.domain.Pet;
 import io.swagger.v3.oas.annotations.Parameter;
 
 @Path("/pet")
-@Produces({"application/json", "application/xml"})
+@Produces({ "application/json", "application/xml" })
 public class PetResource {
 
-  @GET
-  @Path("/{petId}")
-  public Pet getPetById(@PathParam("petId") Long petId)  {
-      // return pet
-      return new Pet();
-  }
+	private Map<Long, Pet> pets = new HashMap<Long, Pet>();
+	
+	@GET
+	public List<Pet> getPets(){
+		return new ArrayList<Pet>(pets.values());
+	}
+	
+	@GET
+	@Path("/{petId}")
+	public Pet getPetById(@PathParam("petId") Long petId) {
+		try {
+			return pets.get(petId);
+		} catch(IndexOutOfBoundsException e) {
+			return new Pet();
+		}
+	}
 
-  @POST
-  @Consumes("application/json")
-  public Response addPet(
-      @Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
-    // add pet
-    return Response.ok().entity("SUCCESS").build();
-  }
+	@POST
+	@Consumes("application/json")
+	public Response addPet(
+			@Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
+		pets.put(pet.getId(), pet);
+		return Response.ok().entity("SUCCESS").build();
+	}
+	
+	@DELETE
+	@Path("/{petId}")
+	public Response delPet(@PathParam("petId") Long id) {
+		pets.remove(id);
+		return Response.ok().entity("SUCCESS").build();
+	}
 }
